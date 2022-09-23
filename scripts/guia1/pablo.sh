ec44fca26e4ae41d7a77d2c153eccb28e3086fb3
Luciana <LuuLuu135@gmail.com>
2022-09-23 14:52:09 -0300

    Guia, permisos y grupos creo que terminado

--- scripts/guia1/pablo.sh
+++ scripts/guia1/pablo.sh
@@ -11,4 +11,34 @@ chmod 700 topssecretsdepablo.txt
 touch amigos.txt
 chmod 770
 
+#Buscamos el archivo kalarm a modo de ejemplo
+cd /usr/bin
+
+find /usr/bin -name kalarm 
+
+#si lo encuentra realiza la copia
+
+cp /usr/bin/kalarm Trabajo/
+
+#ahora me dirijo al directorio Trabajo
+
+cd /home/trabajo
+
+#vemos los permisos
+
+ls -l kalarm
+
+#renombramos el archivo
+
+mv kalarm alarma
+
+#cambiamos los permisos de alarma para que solo lo ejecute el propietario
+
+chmod 700 alarma #chmod rwx
+
+#Con respecto a la ultima pregunta, al no poder ingresar al directorio, no puedo ver el archivo. Al intentar abrir el archivo (con el VI), el contenido de este permanece oculto.
+
+
+
+
 
