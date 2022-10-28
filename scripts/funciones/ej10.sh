5a3f9e36e89fd89bf0f3765eeee2b5f6cdcfc7ea
AlejandroGodoy <alegodoy2013@gmail.com>
2022-10-28 19:24:23 -0300

    modificados y terminados

--- scripts/funciones/ej10.sh
+++ scripts/funciones/ej10.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 function existencia(){
-	
-	id $1| awk '{print $1}' 
+		
+		#id $1| awk '{print $1}' 
+		
+		cat /etc/passwd| tr ':' ' ' >> prueba1.temp
+		cat prueba1.temp| awk '{print $1" "$3}'>> prueba.temp
 
+		grep -w $1 prueba.temp   	
+		rm prueba.temp
+		rm prueba1.temp
+	}
 
-}
-
-exitencia $1
+existencia $1
--- scripts/funciones/ej11.sh
+++ scripts/funciones/ej11.sh
@@ -3,9 +3,10 @@
 function ordenar(){
 	echo "la cantidad de usuarios que hay es: "
 	cat /etc/passwd | wc -l
+	
+	echo
 
-	echo "y sus nombres son"
-
+	echo "y sus nombres son:"
 	cat /etc/passwd |tr ':' ' '| awk '{print $1}'| sort 
 
 }
